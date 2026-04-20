3f8e4db5435e86912246db7d373d682565aa12ab
Noora Liv <j.czinzoll@ingressy.de>
2026-04-20 19:12:28 +0200

    Create globals.py

---
+++ globals.py
@@ -0,0 +1,13 @@
+import board
+CSPin = board.D26
+DIR1 = board.D18
+DIR2 = board.D23
+PWM1 = board.D12
+PWM2 = board.D19
+
+SPICLK = board.SCK
+SPIMOSI = board.MOSI
+SPIMISO = board.MISO
+
+# Globale Variablen
+current_mode = 0
